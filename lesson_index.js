6f38e813aa90320d235d050e77b253fbc7ceb780
bird731217 <69518593+bird731217@users.noreply.github.com>
2025-08-15 16:31:45 +0800

    Add files via upload

--- lesson_index.js
+++ lesson_index.js
@@ -33,16 +33,22 @@ $(document).ready(function () {
         var selectionText = dom.val();
 
         $('#imgTraining').attr('src', 'img/' + selectionText + '.jpg');
+		
+		
+		var currentWK = $('#Week').val();
+		
+		bindingTime(currentWK);
+		
     });
 
-    getTrainingData('325', '1k');
+    
 });
 
 function getTrainingData(speed, distance) {
     $('#ftpZoneTable tbody').empty();
 
-
-
+	var currentWK = $('#Week').val();
+	
     var filterArray = trainingArray.filter(function (item) {
         return item.目標 == speed;
     });
@@ -62,16 +68,20 @@ function getTrainingData(speed, distance) {
         html += `<td style="background-color:${colorSetting[trainingType]};">${trainingType}</td>`;
         html += `<td style="background-color:${colorSetting[trainingType]};">${value.split('：')[1]}</td>`;
 
+		html += `<td style="background-color:${colorSetting[trainingType]};"><span id="${trainingType}"></span></td>`;
         html += '</tr>';
         console.log(html);
     });
-
+		
+	
     $('#ftpZoneTable').append(html);
 }
 
 
 function bidingCurrnetDate()
 {
+	getTrainingData('325', '1k');
+	
 	let currentWK='';
 	let now = new Date();
 	let currentMMdd = `${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}`;
@@ -87,8 +97,41 @@ function bidingCurrnetDate()
 	{
 		currentWK = mappingArray[0].wk;	
 	}	
-	
+		
+	bindingTime(currentWK);
 	
 	$('#Week').val(currentWK);
-	$('#imgTraining').attr('src', 'img/' + currentWK+ '.jpg');
+	$('#imgTraining').attr('src', 'img/' + currentWK+ '.jpg');	
+}
+
+function bindingTime(currentWK)
+{
+	var timeSummary = weekDetail.filter(function (item) {
+			return item.week == currentWK;
+	});
+		
+	$('#短間歇').text('');
+	if(timeSummary[0]["短間歇"])
+	{
+		$('#短間歇').text(timeSummary[0]['短間歇']);
+	}
+	
+	$('#節奏跑').text('');
+	if(timeSummary[0]["節奏跑"])
+	{
+		$('#節奏跑').text(timeSummary[0]['節奏跑']);
+	}
+	
+	
+	$('#長跑').text('');
+	if(timeSummary[0]["長跑"])
+	{
+		$('#長跑').text(timeSummary[0]['長跑']);
+	}
+	
+	$('#長間歇').text('');
+	if(timeSummary[0]["長間歇"])
+	{
+		$('#長間歇').text(timeSummary[0]['長間歇']);
+	}
 }
